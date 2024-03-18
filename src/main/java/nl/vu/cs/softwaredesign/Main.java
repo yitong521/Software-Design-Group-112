2b4f11ab4653cc80e4013e0234dec2f393f633fd
yitong521 <52449332+yitong521@users.noreply.github.com>
2024-03-18 07:34:54 +0100

    Update Main.java

--- src/main/java/nl/vu/cs/softwaredesign/Main.java
+++ src/main/java/nl/vu/cs/softwaredesign/Main.java
@@ -1,7 +1,32 @@
-package nl.vu.cs.softwaredesign;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main (String[] args){
-        System.out.println("Welcome to Software Design!");
+public class HelloWorld extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // 创建一个 Text 对象
+        Text helloText = new Text("Hello, JavaFX!");
+
+        // 创建一个 StackPane，并将 Text 放置在其中
+        StackPane root = new StackPane();
+        root.getChildren().add(helloText);
+
+        // 创建一个 Scene，并将 StackPane 放置在其中
+        Scene scene = new Scene(root, 300, 200);
+
+        // 将 Scene 设置到 Stage 上，并设置 Stage 的标题
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX Hello World");
+
+        // 显示 Stage
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
